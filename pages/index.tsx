8175fb7299c5abca90c9acc5d4952b756919b3de
Ankit Tiwari <ankitt255@gmail.com>
2020-12-13 21:50:08 +0530

    :fire: Remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
-import { DownArrow } from "src/componets/DownArrow";
 import { Footer } from "src/componets/Footer";
 import { GetInTouch } from "src/componets/GetInTouch";
 import { Header } from "src/componets/header";
 import { Introduction } from "src/componets/Introduction";
 import { JumboTron } from "src/componets/Jumbotron";
 import { Projects } from "src/componets/Projects";
-import { SectionHeader } from "src/componets/SectionHeader";
 
 export default function IndexPage() {
   return (
