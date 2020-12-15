3cdd842c6aa9d7083dd673c1dabb72d44273c248
Ankit Tiwari <ankitt255@gmail.com>
2020-12-15 13:17:38 +0530

    :bug: Fix absolute path

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
-import { Footer } from "src/componets/Footer";
-import { GetInTouch } from "src/componets/GetInTouch";
-import { Header } from "src/componets/header";
-import { Introduction } from "src/componets/Introduction";
-import { JumboTron } from "src/componets/Jumbotron";
-import { Projects } from "src/componets/Projects";
+import { Footer } from "../src/componets/Footer";
+import { GetInTouch } from "../src/componets/GetInTouch";
+import { Header } from "../src/componets/Header";
+import { Introduction } from "../src/componets/Introduction";
+import { JumboTron } from "../src/componets/Jumbotron";
+import { Projects } from "../src/componets/Projects";
 
 export default function IndexPage() {
   return (
