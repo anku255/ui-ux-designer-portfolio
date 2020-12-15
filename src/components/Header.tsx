6f359627c31152f3d6224455a8b49928b7484e33
Ankit Tiwari <ankitt255@gmail.com>
2020-12-15 18:48:32 +0530

    :sparkles: Add sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
-export const Header = () => (
+import { useState } from "react";
+
+export const HeaderWeb = () => (
   <header className="fixed top-0 left-0 z-10 hidden w-full text-sm tracking-wide uppercase border-b-2 border-white header bg-white-70 h-15 sm:block">
     <nav className="h-full">
       <ul className="flex items-center justify-center w-full h-full space-x-36">
@@ -38,4 +40,127 @@ export const Header = () => (
       }
     `}</style>
   </header>
+);
+
+const Sidebar = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setIsSidebarOpen((isOpen) => !isOpen);
+
+  return (
+    <div className="relative sm:hidden">
+      <button onClick={toggleSidebar}>
+        <span className={`hamburger ${isSidebarOpen ? "open" : ""}`}>
+          &nbsp;
+        </span>
+      </button>
+
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <nav className="h-full">
+          <ul className="flex flex-col items-center justify-center h-full text-sm tracking-wide uppercase space-y-18">
+            <li>
+              <a href="#projects" onClick={toggleSidebar}>
+                Projects
+              </a>
+            </li>
+            <li>
+              <a
+                href="/files/Resume.pdf"
+                target="__blank"
+                onClick={toggleSidebar}
+              >
+                Resume
+              </a>
+            </li>
+            <li>
+              <a href="#contact" onClick={toggleSidebar}>
+                Contact
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <style jsx>{`
+        button {
+          background: rgb(191, 241, 237);
+          opacity: 0.75;
+          height: 42px;
+          width: 42px;
+          position: fixed;
+          top: 1rem;
+          right: 1rem;
+          border-radius: 50%;
+          z-index: 2001;
+          box-shadow: 0 0.5rem 2rem rgba(0, 0, 0, 0.3);
+          text-align: center;
+          cursor: pointer;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        button:focus {
+          outline: none;
+        }
+
+        .hamburger {
+          margin-top: -8px;
+          position: relative;
+          width: 1rem;
+          height: 2px;
+          background: #707070;
+          transition: all 0.2s;
+        }
+
+        .hamburger:after {
+          content: "";
+          position: absolute;
+          top: 8px;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          background: #707070;
+          transition: all 0.2s;
+        }
+
+        .hamburger.open {
+          margin-top: -6px;
+          transform: rotate(45deg) translate(2px, 2px);
+          top: 0;
+          left: 0;
+        }
+
+        .hamburger.open:after {
+          transform: rotate(-90deg) translate(8px, 0px);
+        }
+
+        .sidebar {
+          position: fixed;
+          top: 0;
+          right: 0;
+          width: 15rem;
+          height: 100vh;
+          height: calc(100vh - 60);
+          height: calc(var(--vh, 1vh) * 100);
+          background: rgba(179, 229, 225, 0.5);
+          backdrop-filter: blur(10px);
+          z-index: 2000;
+          transform: translateX(15rem);
+          transition: all 0.3s ease-out;
+        }
+
+        .sidebar.open {
+          transform: translateX(0);
+        }
+      `}</style>
+    </div>
+  );
+};
+
+export const Header = () => (
+  <>
+    <HeaderWeb />
+    <Sidebar />
+  </>
 );
