4fc2eb5122c3d8e6ba1541710426222746b5dee9
Ankit Tiwari <ankitt255@gmail.com>
2020-12-16 00:08:18 +0530

    :sparkles: Add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import { useEffect } from "react";
 import Head from "next/head";
 import "../src/styles/index.css";
 
+const WEBSITE_URL = "https://ekanshi.design";
+
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
     // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
@@ -18,9 +20,42 @@ function MyApp({ Component, pageProps }) {
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, []);
+
   return (
     <div>
       <Head>
+        <title>Ekanshi Kiran | Product Designer</title>
+        <meta
+          name="description"
+          content="Portfolio website of Ekanshi Kiran. You can find her on behance @ekanshikiran"
+        />
+        <meta name="keywords" content="UI/UX Desginer, Product Designer" />
+        <meta property="og:title" content="Ekanshi Kiran | Product Designer" />
+        <meta
+          property="og:description"
+          content="Portfolio website of Ekanshi Kiran. You can find her on behance @ekanshikiran"
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content={`${WEBSITE_URL}/images/og-image.png`}
+        />
+        <meta property="og:image:type" content="image/png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="627" />
+        <meta property="og:url" content={`${WEBSITE_URL}`} />
+        <meta property="og:site_name" content="Ekanshi Kiran" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Ekanshi Kiran | Product Designer" />
+        <meta
+          name="twitter:description"
+          content="Portfolio website of Ekanshi Kiran. You can find her on behance @ekanshikiran"
+        />
+        <meta
+          name="twitter:image"
+          content={`${WEBSITE_URL}/images/og-image.png`}
+        />
+
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
