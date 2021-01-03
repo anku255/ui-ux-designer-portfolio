8fd1ae3456bdbb66b3492f9eb98a6707bb05eea6
Ankit Tiwari <ankitt255@gmail.com>
2021-01-03 22:29:28 +0530

    :memo: Update project info

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { DownArrow } from "./DownArrow";
 import { SectionHeader } from "./SectionHeader";
 
@@ -6,31 +7,37 @@ const Project = ({
   title,
   description,
   imageUrl,
+  projectUrl,
 }: {
   title: string;
   description: string;
   imageUrl: string;
+  projectUrl: string;
 }) => (
-  <div className="px-6 mx-auto sm:px-24 sm:h-112">
-    <div className="flex flex-col sm:flex-row sm:max-h-240">
-      {/* Left */}
-      <div className="relative self-start order-3 w-full p-10 mr-16 bg-white sm:order-1 sm:w-1/2 hover-bg-animation">
-        <div className="text-5xl font-cursive">{title}</div>
-        <div className="h-8"></div>
-        <div className="text-xl">{description}</div>
+  <Link href={projectUrl}>
+    <a target="_blank" rel="noopener noreferrer">
+      <div className="px-6 mx-auto sm:px-24 sm:h-112">
+        <div className="flex flex-col sm:flex-row sm:max-h-240">
+          {/* Left */}
+          <div className="relative self-start order-3 w-full p-10 mr-16 bg-white sm:order-1 sm:w-1/2 hover-bg-animation">
+            <div className="text-5xl font-cursive">{title}</div>
+            <div className="h-8"></div>
+            <div className="text-xl">{description}</div>
+          </div>
+          {/* Right */}
+          <div className="flex justify-center order-2 w-full h-full mb-6 sm:mb-0 sm:w-1/2 sm:h-1/2">
+            <Image
+              src={imageUrl}
+              alt={title}
+              width={450}
+              height={450}
+              layout="intrinsic"
+            />
+          </div>
+        </div>
       </div>
-      {/* Right */}
-      <div className="flex justify-center order-2 w-full h-full mb-6 sm:mb-0 sm:w-1/2 sm:h-1/2">
-        <Image
-          src={imageUrl}
-          alt={title}
-          width={450}
-          height={450}
-          layout="intrinsic"
-        />
-      </div>
-    </div>
-  </div>
+    </a>
+  </Link>
 );
 
 export const Projects = () => (
@@ -46,20 +53,23 @@ export const Projects = () => (
     {/* Projects */}
     <Project
       title="Go Flatmates"
-      description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+      description="AI-powered millennial housing and lifestyle platform that allows users to connect users to connect with the right people at the right location, to solve their housing and living requirements."
       imageUrl="/images/desktop/project1.png"
+      projectUrl="https://www.behance.net/gallery/102567267/Graduation-project-Goflatmatescom"
     />
     <div className="h-10 sm:h-24"></div>
     <Project
-      title="Cron J"
-      description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+      title="AnkuCodes"
+      description="A passionate software engineer portfolio, showing a diverse range of projects and Tech blogs. Check out the live website here."
       imageUrl="/images/desktop/project2.png"
+      projectUrl="https://ankucodes.netlify.app/"
     />
     <div className="h-10 sm:h-24"></div>
     <Project
       title="Quotic"
-      description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
+      description="A new place to find all your favourite series & movie quotes, or simply find your favorite characters' best dialogues here. Check the live project."
       imageUrl="/images/desktop/project3.png"
+      projectUrl="https://quotic.vercel.app/"
     />
     <div className="h-16"></div>
     <div className="flex justify-center ">
