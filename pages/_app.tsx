35a3fb55cdd22a76de74f9bb834818d19b4a8908
Ankit Tiwari <ankucodes@gmail.com>
2023-09-22 23:13:41 +0530

    Fix package version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -59,11 +59,7 @@ function MyApp({ Component, pageProps }) {
         <link rel="shortcut icon" href="/images/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;700&family=Libre+Baskerville:wght@400;700&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
